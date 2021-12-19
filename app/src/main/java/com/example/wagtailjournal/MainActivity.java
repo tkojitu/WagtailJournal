901e9074866e6af2c7e764a4b7c1224c3caa5be5
aka. bakaiku or @gdgdiler <greentea@fa2.so-net.ne.jp>
2021-12-19 21:56:33 +0900

    request permission.

--- app/src/main/java/com/example/wagtailjournal/MainActivity.java
+++ app/src/main/java/com/example/wagtailjournal/MainActivity.java
@@ -1,37 +1,20 @@
 package com.example.wagtailjournal;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.provider.Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION;
 
 public class MainActivity extends AppCompatActivity {
-    private class OyajiFactory implements ServiceFactory {
-        @Override
-        public Object create(Container co) {
-            return new Oyaji((Kakaa)co.geti("kakaa"));
-        }
-    }
-
-    private class KakaaFactory implements ServiceFactory {
-        @Override
-        public Object create(Container co) {
-            return new Kakaa((Musuko)co.geti("musuko"), (Musume)co.geti("musume"));
-        }
-    }
-
-    private class MusukoFactory implements ServiceFactory {
-        @Override
-        public Object create(Container co) {
-            return new Musuko();
-        }
-    }
-
-    private class MusumeFactory implements ServiceFactory {
-        @Override
-        public Object create(Container co) {
-            return new Musume();
-        }
-    }
+    static final int APP_STORAGE_ACCESS_REQUEST_CODE = 1;
 
     private Container container = new Container();
 
@@ -43,9 +26,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupContainer() {
-        container.defserv("oyaji", new OyajiFactory())
-                .defserv("kakaa", new KakaaFactory())
-                .defserv("musuko", new MusukoFactory())
-                .defserv("musume", new MusumeFactory());
+        container.defserv("oyaji",
+                new ServiceFactory() {
+                    public Object create(Container co) {
+                        return new Oyaji((Kakaa)co.geti("kakaa"));
+                    }
+                })
+        .defserv("kakaa",
+                new ServiceFactory() {
+                    public Object create(Container co) {
+                        return new Kakaa((Musuko)co.geti("musuko"), (Musume)co.geti("musume"));
+                    }
+                })
+        .defserv("musuko",
+                new ServiceFactory() {
+                    public Object create(Container co) {
+                        return new Musuko();
+                    }
+                })
+        .defserv("musume",
+                new ServiceFactory() {
+                    public Object create(Container co) {
+                        return new Musume();
+                    }
+                });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public void onClick(View view) {
+        if (!Environment.isExternalStorageManager())  {
+            requestPermission();
+            return;
+        }
+    }
+
+    private void requestPermission() {
+        Uri uri = Uri.parse("package:" + BuildConfig.APPLICATION_ID);
+        Intent intent = new Intent(ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION, uri);
+        startActivityForResult(intent, APP_STORAGE_ACCESS_REQUEST_CODE);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == APP_STORAGE_ACCESS_REQUEST_CODE) {
+            if (Environment.isExternalStorageManager())  {
+                Toast.makeText(this, "permision granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "permision denied", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
